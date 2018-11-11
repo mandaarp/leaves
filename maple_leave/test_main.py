ea2242d7b5d1cbc602bd09d2b573ca1b636273c6
Mandar Patil <mandaarp@gmail.com>
2018-11-11 09:04:57 -0800

    added test for print values - pytest running in buffered mode;

--- maple_leave/test_main.py
+++ maple_leave/test_main.py
@@ -1,6 +1,6 @@
 __author__ = 'mandar'
 
-import unittest
+import unittest, sys
 from maple_leave import main
 
 class TestMapleLeave(unittest.TestCase):
@@ -14,3 +14,13 @@ class TestMapleLeave(unittest.TestCase):
     def test_load_files(self):
         actual_list = main.load_files(path=main.TEST_FOLDER)
         self.assertEqual(len(actual_list), 1)
+
+    def test_print_values(self):
+        expected_tag = 'tag'
+        expected_original_text = 'original'
+        expected_new_text = 'new'
+        main.print_values(tag=expected_tag, original_text=expected_original_text, new_text=expected_new_text)
+        if not hasattr(sys.stdout, 'getvalue'):
+            self.fail('Need to run in buffered mode.')
+        actual_output = sys.stdout.getvalue().strip()
+        self.assertEqual(actual_output, '{0} > {1} > {2}'.format(expected_tag, expected_original_text, expected_new_text))
